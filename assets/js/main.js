bae98e391e792a49e5ac378da7c047936bed2555
uk-support1 <chaburin929@gmail.com>
2026-08-06 17:21:19 +0900

    比較サイトのトップページを実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+(function () {
+  var navToggle = document.querySelector(".nav-toggle");
+  var globalNav = document.querySelector(".global-nav");
+
+  if (!navToggle || !globalNav) {
+    return;
+  }
+
+  navToggle.addEventListener("click", function () {
+    var isOpen = globalNav.classList.toggle("is-open");
+    navToggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
+  });
+})();
