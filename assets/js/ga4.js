94ce341ba433a1b859c9a0db0502bcedebf1fe08
uk-support1 <chaburin929@gmail.com>
2026-08-06 20:57:02 +0900

    GA4タグを各ページへ直接設置

--- assets/js/ga4.js
+++ assets/js/ga4.js
@@ -1,25 +1,3 @@
-"use strict";
-
-(function () {
-  var GA_MEASUREMENT_ID = "G-KNYX0KWLY";
-
-  if (window.dataLayer && window.dataLayer.__ga4Initialized) {
-    return;
-  }
-
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.__ga4Initialized = true;
-
-  function gtag() {
-    window.dataLayer.push(arguments);
-  }
-  window.gtag = gtag;
-
-  gtag("js", new Date());
-  gtag("config", GA_MEASUREMENT_ID);
-
-  var script = document.createElement("script");
-  script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_MEASUREMENT_ID;
-  document.head.appendChild(script);
-})();
+// GA4は各HTMLのhead内へGoogle公式標準タグを直接設置している
+// 測定ID: G-KNYX0KWLY
+// 二重設置を防ぐため、このファイルからは読み込まない
