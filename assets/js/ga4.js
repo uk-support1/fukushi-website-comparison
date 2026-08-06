1bb59577629d50733caf69ebe3e0aab63d03ace8
uk-support1 <chaburin929@gmail.com>
2026-08-06 18:14:46 +0900

    比較サイト専用のGA4を設定

--- assets/js/ga4.js
+++ assets/js/ga4.js
@@ -1,2 +1,25 @@
-// GA4測定ID: 未設定（比較サイト専用の測定IDが確定次第記載）
-// 実際のGA4タグは未実装
+"use strict";
+
+(function () {
+  var GA_MEASUREMENT_ID = "G-KNYX0KWLY";
+
+  if (window.dataLayer && window.dataLayer.__ga4Initialized) {
+    return;
+  }
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.__ga4Initialized = true;
+
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  window.gtag = gtag;
+
+  gtag("js", new Date());
+  gtag("config", GA_MEASUREMENT_ID);
+
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_MEASUREMENT_ID;
+  document.head.appendChild(script);
+})();
